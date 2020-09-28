7abd16208c8923d423d6da6610a1cda9ba15c699
marcosaleL <alelazarte55@gmail.com>
2020-09-28 18:06:19 -0300

    Leer un archivo de texto linea por linea

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 import AnalizadorLexico.LectorBuffer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		String cadena = "";
-		LectorBuffer leeArchivo;
-		String path = "c:\\Users\\ale_5\\git\\Trabajo-Compiladores\\prueba.txt"
-		leeArchivo = new LectorBuffer(new InputStreamReader(new FileInputStream(path));
-		cadena = leerArchivo.readLine();
-		while(cadena != null) {
+	public static void main(String[] args) throws IOException {
+		/* -----Para leer linea por linea de un archivo
+		 * String cadena = "";
+		
+		LectorBuffer leerArchivo;
+		String path = "c:\\Users\\ale_5\\git\\Trabajo-Compiladores\\prueba.txt";
+		leerArchivo = new LectorBuffer(new InputStreamReader(new FileInputStream(path)));
+		while((cadena=leerArchivo.readLine()) != null) {
 	          System.out.println(cadena);
+	     */
 	    }
-	}
-
 }
+
